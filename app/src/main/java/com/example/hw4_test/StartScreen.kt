579b0c2d9d4d328acc39a33586d9be0ec53f1992
Kobe Johnson <kobejohnson7771@gmail.com>
2021-10-04 11:15:07 -0400

    Added the Start Screen Activity and the UI for it.

---
+++ app/src/main/java/com/example/hw4_test/StartScreen.kt
@@ -0,0 +1,11 @@
+package com.example.hw4_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_screen)
+    }
+}
