3ffa25d6b7e82ae6abaab679fd05294e0eeee356
ewannemacher13 <ewannemacher13@gmail.com>
2021-10-04 12:03:54 -0400

    Add seekBar and rotating player turn

--- app/src/main/java/com/example/hw4_test/MainActivity.kt
+++ app/src/main/java/com/example/hw4_test/MainActivity.kt
@@ -4,16 +4,20 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TableLayout
+import android.widget.SeekBar
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.gridlayout.widget.GridLayout
-import kotlin.random.Random
 
 class Board(val gridLayout: GridLayout, val context: Context) {
     val board : MutableList<MutableList<BoardItem>> = ArrayList()
+    var playerTurn : Int = 1
+    var numPlayers = 2
+
+    constructor(gridLayout: GridLayout, context: Context, numPlayers: Int) : this(gridLayout, context) {
+        this.numPlayers = numPlayers
+    }
 
     init {
 
@@ -56,7 +60,7 @@ class Board(val gridLayout: GridLayout, val context: Context) {
         this.gridLayout.addView(textView)
     }
 
-    fun move(piece : Int, column: Int) {
+    fun move(column: Int, piece: Int = this.playerTurn): Int {
         var testRow = this.gridLayout.rowCount - 1
 
         // get the lowest empty tile or 0
@@ -64,7 +68,11 @@ class Board(val gridLayout: GridLayout, val context: Context) {
             testRow--
         }
 
-        this.board[testRow][column].fillSpot(piece)
+        if (this.board[testRow][column].fillSpot(piece)) {
+            this.playerTurn = getNextPlayer(piece)
+        }
+
+        return this.playerTurn
     }
 
     fun clearBoard() {
@@ -74,6 +82,11 @@ class Board(val gridLayout: GridLayout, val context: Context) {
             }
         }
     }
+
+    fun getNextPlayer(currentPlayer: Int = this.playerTurn): Int {
+        return (currentPlayer ) % this.numPlayers + 1
+    }
+
 }
 
 class BoardItem(val tv: TextView) {
@@ -100,6 +113,10 @@ class BoardItem(val tv: TextView) {
                 this.tv.setBackgroundResource(R.drawable.ic_yellow_circle)
                 this.state = piece
             }
+            3 -> {
+                this.tv.setBackgroundResource(R.drawable.ic_green_circle)
+                this.state = piece
+            }
             else -> {
                 this.tv.setBackgroundResource(R.drawable.ic_white_circle)
                 this.state = 0
@@ -116,26 +133,53 @@ class MainActivity : AppCompatActivity() {
 
         val gridLayout = findViewById<GridLayout>(R.id.gridLayout)
         val textView = findViewById<TextView>(R.id.textView)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
 
         gridLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.black))
         gridLayout.columnCount = 9
         gridLayout.rowCount = 6
 
-        val board = Board(gridLayout,this)
+        val players = mapOf(1 to R.color.red, 2 to R.color.yellow)
+//        val players = mapOf(1 to R.color.red, 2 to R.color.yellow, 3 to R.color.green)
+
+        val board = Board(gridLayout,this, players.size)
 
         var j = 0
+
+        seekBar.max = board.gridLayout.columnCount - 1
+        seekBar.progress = j
+        seekBar?.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar?, value: Int, fromUser: Boolean) {
+                j = value
+
+                textView.text = j.toString()
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar?) {
+            }
+        })
 
         // red
         val button = findViewById<Button>(R.id.button)
         button?.setOnClickListener {
-
-            board.move(1,j)
+            board.move(j, 1)
         }
 
         // yellow
         val button2 = findViewById<Button>(R.id.button2)
         button2?.setOnClickListener {
-            board.move(2, j)
+            board.move(j, 2)
+        }
+
+        // nextPlayer
+        val turn = findViewById<Button>(R.id.turn)
+        turn.setBackgroundColor(ContextCompat.getColor(this, players[1] ?: R.color.red))
+        turn?.setOnClickListener {
+            val nextPlayer: Int = board.move(j)
+            turn.setBackgroundColor(ContextCompat.getColor(this, players[nextPlayer] ?: R.color.red))
         }
 
         // right
@@ -144,7 +188,8 @@ class MainActivity : AppCompatActivity() {
 
             j = if (j < board.gridLayout.columnCount-1) j+1 else 0
 
-            textView.text = j.toString()
+            seekBar.progress = j
+
         }
 
         // left
@@ -153,7 +198,7 @@ class MainActivity : AppCompatActivity() {
 
             j = if (j > 0 ) j-1 else board.gridLayout.columnCount - 1
 
-            textView.text = j.toString()
+            seekBar.progress = j
 
         }
 
