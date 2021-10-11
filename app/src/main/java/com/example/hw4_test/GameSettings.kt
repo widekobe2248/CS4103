cad4dfb8a6db679d95e09903ca3ae34e1ccd9ae1
Kobe Johnson <kobejohnson7771@gmail.com>
2021-10-11 11:25:24 -0400

    Fixed Drop Down and got Intent's Working and sending data

--- app/src/main/java/com/example/hw4_test/GameSettings.kt
+++ app/src/main/java/com/example/hw4_test/GameSettings.kt
@@ -4,10 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
+import android.widget.*
 
 class GameSettings : AppCompatActivity(), View.OnClickListener, AdapterView.OnItemSelectedListener {
 
@@ -16,6 +13,8 @@ class GameSettings : AppCompatActivity(), View.OnClickListener, AdapterView.OnIt
     var board_width = 9
     var board_height = 5
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_settings)
@@ -23,24 +22,27 @@ class GameSettings : AppCompatActivity(), View.OnClickListener, AdapterView.OnIt
         var start_game = findViewById<Button>(R.id.start_game_btn)
 
 
-        //Handles Drop-Down Menu for player amount
-        var player_options: Spinner = findViewById(R.id.player_amt)
-        ArrayAdapter.createFromResource(this, R.array.player_amount, android.R.layout.simple_spinner_dropdown_item).also {
-            adapter -> adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            player_options.adapter = adapter
-        }
+
+
+
+        var player_options = findViewById<Spinner>(R.id.player_amt)
+        var adapter = ArrayAdapter.createFromResource(this, R.array.player_amount, android.R.layout.simple_spinner_dropdown_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        player_options.adapter = adapter
         player_options.onItemSelectedListener = this
 
-        var connection_win: Spinner = findViewById(R.id.connection_win_amt)
-        ArrayAdapter.createFromResource(this, R.array.connection_win_amount, android.R.layout.simple_spinner_dropdown_item).also {
-            adapter -> adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            player_options.adapter = adapter
-        }
-        connection_win.onItemSelectedListener = this
+
+
+        var connections_win = findViewById<Spinner>(R.id.connection_win_amt)
+        var adapter1 = ArrayAdapter.createFromResource(this, R.array.connection_win_amount, android.R.layout.simple_spinner_dropdown_item)
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        connections_win.adapter = adapter1
+        connections_win.onItemSelectedListener = this
 
 
         start_game.setOnClickListener(this)
     }
+
 
     override fun onItemSelected(parent: AdapterView<*>, v: View?, pos: Int, id: Long) {
         //Has to be converted to string before casting to Int
@@ -68,6 +70,10 @@ class GameSettings : AppCompatActivity(), View.OnClickListener, AdapterView.OnIt
     override fun onClick(v: View) {
         if(v.id == R.id.start_game_btn)
         {
+            var widthBoardSizeInput = findViewById<EditText>(R.id.widthText)
+            var lengthBoardSizeInput = findViewById<EditText>(R.id.lengthText)
+            board_width = widthBoardSizeInput.text.toString().toInt()
+            board_height = lengthBoardSizeInput.text.toString().toInt()
             val intent = Intent(this, MainActivity::class.java)
             intent.putExtra("player_amt", player_amount)
             intent.putExtra("connection_win", connections_win_amt )
